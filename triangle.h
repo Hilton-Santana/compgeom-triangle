7a80510085091cfb4cc2a19a38ddcd368ed413d0
Hilton-Marques <hiltonmarquess@gmail.com>
2025-03-30 17:14:12 -0300

    We dont need the other type of triangulate by now

--- triangle.h
+++ triangle.h
@@ -284,11 +284,6 @@ struct triangulateio {
   int numberofedges;                                             /* Out only */
 };
 
-#ifdef ANSI_DECLARATORS
 void triangulate(char *, struct triangulateio *, struct triangulateio *,
                  struct triangulateio *);
 void trifree(void *memptr);
-#else /* not ANSI_DECLARATORS */
-void triangulate();
-void trifree();
-#endif /* not ANSI_DECLARATORS */
